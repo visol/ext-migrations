de0c92954c6a7d9e644a22ad199736088435725c
Daniel Huf <dhuf@users.noreply.github.com>
2022-03-29 19:48:04 +0200

    Update composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Doctrine Migrations',
     'description' => 'Enable SQL migration powered by Doctrine',
     'category' => 'service',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'state' => 'stable',
     'author' => 'Kay Strobach, Fabien Udriot',
     'author_email' => 'typo3@kay-strobach.de, fabien.udriot@visol.ch',
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.00-10.4.99'
+            'typo3' => '10.4.00-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
