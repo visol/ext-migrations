a80117b21105e50f460c1d9c48d89fe81a1d81a4
Fabien Udriot <fabien@omic.ch>
2022-06-17 16:08:11 +0200

    task: streamline ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,17 +3,8 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Doctrine Migrations',
     'description' => 'Enable SQL migration powered by Doctrine',
-    'category' => 'service',
-    'version' => '1.0.1',
     'state' => 'stable',
     'author' => 'Kay Strobach, Fabien Udriot',
     'author_email' => 'typo3@kay-strobach.de, fabien.udriot@visol.ch',
-    'author_company' => '',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.00-11.5.99'
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
+    'author_company' => 'visol - digitale Dienstleistungen GmbH',
 ];
